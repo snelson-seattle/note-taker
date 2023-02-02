7dff2ea989116801052a601fd36a32e0b61b5ce2
Scott Nelson <snelson@instructors.2u.com>
2023-02-02 14:23:35 -0800

    finished GET route for /api/notes, it is successfully returning all of the notes in db.json

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
 
 router.get("/notes", (req, res) => {
-  res.status(200).json({ message: "success" });
+  fs.readFile(
+    path.join(__dirname, "../../db/db.json"),
+    "utf-8",
+    (error, data) => {
+      if (error) {
+        res.status(500).json({ error: error });
+      } else {
+        const notes = JSON.parse(data);
+        res.status(200).json(notes);
+      }
+    }
+  );
 });
 
 router.post("/notes", (req, res) => {
